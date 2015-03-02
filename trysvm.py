022720151574e7c58c9f65f7ae8549c405491bf4
WANG Longqi <iqgnol@gmail.com>
2015-03-02 20:14:43 +0800

    add rotation

--- trysvm.py
+++ trysvm.py
@@ -6,6 +6,11 @@ import sklearn.svm as svm
 from sklearn.metrics import accuracy_score
 from skimage import measure ,morphology
 from numpy import *
+import numpy as np
+# add rotate function from
+# https://github.com/nicholaslocascio/kaggle-mnist-digits/blob/master/helpers.py
+import scipy.ndimage as nd
+
 
 # Read in data
 train = pd.read_csv('train.csv')
@@ -15,9 +20,25 @@ train.drop('label', axis=1, inplace=True)
 X = train.values
 y = y.values
 
+
+def rotate_dataset(X, Y, n_rotations=2):
+    for rot_i in range(n_rotations):
+        rot_shape = (X.shape[0], X.shape[1])
+        rot_X = np.zeros(rot_shape)
+        for index in range(X.shape[0]):
+            sign = random.choice([-1, 1])
+            angle = np.random.randint(1, 12)*sign
+            rot_X[index, :] = (nd.rotate(np.reshape(X[index, :], ((28, 28))), angle, reshape=False).ravel())
+        XX = np.vstack((X,rot_X))
+        YY = np.hstack((Y,Y))
+    return XX, YY
+
+
 # Preprocessing
 for ind in range(X.shape[0]):
 	X[ind,:] = where(X[ind,:] > mean(X[ind,:]),0.,1.)
+
+X, y = rotate_dataset(X, y, 10)
 
 # PCA
 pca = PCA(50) 
@@ -25,8 +46,11 @@ pca.fit(X)
 X50 = pca.transform(X)
 
 # SVC
-svc = svm.SVC(cache_size=2000)
+svc = svm.SVC(C=1, cache_size=2000)
 svc.fit(X50,y)
+
+# Recall
+print accuracy_score(y,svc.predict(X50))
 
 # Read in test data
 test = pd.read_csv('test.csv')
@@ -49,6 +73,9 @@ pd.Series(p,range(1,p.size+1)).to_csv('svc.csv')
 # NuSVC
 nusvc = svm.NuSVC(nu=0.01,cache_size=2000)
 nusvc.fit(X50,y)
+
+# Recall
+print accuracy_score(y,nusvc.predict(X50))
 
 # Predict
 p = nusvc.predict(test)
